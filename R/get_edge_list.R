e9ae1490eb97fc6db06a8cae486b40abed9a53fd
Michael Davies <michaelldavies@gmail.com>
2023-02-19 15:39:44 -0500

    add as.symbol, drop pick

--- R/get_edge_list.R
+++ R/get_edge_list.R
@@ -4,17 +4,17 @@
 #'
 #' @details \code{get_edge_list()} takes an edge list with an id column where the second column has multiple values. It then separates the second column and creates a full edge list. As opposed to \code{separate_rows()}, this function only keeps those pairwise combinations that exist in the original data.
 #'
-#' @param df dataframe
-#' @param actor actor or node
-#' @param assoc_actor_1 second actor or node with multiple values sep = ';'
-#' @param idx index, row number or grouping variable
-#' @param sep separator - default sep = ';'
+#' @param df A dataframe
+#' @param actor Actor or node
+#' @param assoc_actors Second actor or node with multiple values
+#' @param idx Index, row number or grouping variable
+#' @param sep Separator delimiting collapsed values - default sep = ';'
 #'
 #' @return returns a dataframe or tibble
 #'
 #' @examples
 #' df <- tibble::tibble(
-#' actor_1 = c("A", "A", "B"),
+#'  actor_1 = c("A", "A", "B"),
 #'  assoc_actor_1 = c("B", "B; C", "C; D; E")) |>
 #'    dplyr::mutate(idx = dplyr::row_number()) |>
 #'    dplyr::select(idx, dplyr::everything())
@@ -25,6 +25,7 @@
 #'                     idx = 'idx'))
 #'
 #' @importFrom magrittr %>%
+#' @import rlang
 #'
 #' @export
 get_edge_list <- function(df,
@@ -33,30 +34,32 @@ get_edge_list <- function(df,
                           idx,
                           sep = ';'
                           ){
-  df_temp <- df  |>
-    dplyr::select(all_of(actor), all_of(assoc_actors), all_of(idx)) |>
-    dplyr::group_by(idx) |>
-    dplyr::filter(stringr::str_detect(assoc_actors, ';')) |>
-    tidyr::separate_rows(assoc_actors) |>
-    dplyr::mutate(assoc_actors = stringr::str_squish(assoc_actors)) |>
-    tidyr::pivot_longer(-idx) |>
-    dplyr::select(value) |>
-    dplyr::distinct(value) |>
-    dplyr::mutate(value2 = value) |>
-    tidyr::expand(value, value2) |>
-    dplyr::filter(value !=value2) |>
-    dplyr::mutate(helper = stringr::str_c(value,value2)) |>
-    dplyr::rowwise() |>
+  df_temp <- df %>%
+    dplyr::select(as.symbol(actor),
+                  as.symbol(assoc_actors),
+                  as.symbol(idx)) %>%
+    dplyr::group_by(idx) %>%
+    dplyr::filter(stringr::str_detect(eval(as.symbol(assoc_actors)), sep)) %>%
+    tidyr::separate_rows(dplyr::all_of(assoc_actors)) %>%
+    # dplyr::mutate(!!assoc_actors := stringr::str_squish(!!rlang::enquo(assoc_actors))) %>%
+    tidyr::pivot_longer(!!actor:!!assoc_actors) %>%
+    dplyr::select(value) %>%
+    dplyr::distinct(value) %>%
+    dplyr::mutate(value2 = value) %>%
+    tidyr::expand(value, value2) %>%
+    dplyr::filter(value != value2) %>%
+    dplyr::mutate(helper = stringr::str_c(value, value2)) %>%
+    dplyr::rowwise() %>%
     dplyr::mutate(helper = stringr::str_c(
-      stringr::str_sort(unlist(stringr::str_split(helper, ""))),collapse = "")) |>
-    dplyr::distinct(helper,.keep_all = T) |>
-    dplyr::select(-helper) |>
-    dplyr::rename(actor = value, assoc_actors = value2) |>
+      stringr::str_sort(unlist(stringr::str_split(helper, ""))),collapse = "")) %>%
+    dplyr::distinct(helper,.keep_all = T) %>%
+    dplyr::select(-helper) %>%
+    dplyr::rename(!!actor := value, !!assoc_actors := value2) %>%
     dplyr::ungroup()
 
   #return(df_temp)
 
-  return <- df |>
-    dplyr::filter(!stringr::str_detect(assoc_actors, ';')) |>
+  return <- df %>%
+    dplyr::filter(!stringr::str_detect(eval(as.symbol(assoc_actors)), sep)) %>%
     dplyr::bind_rows(df_temp)
 }
